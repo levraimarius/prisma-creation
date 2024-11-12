ac18e67ffcc30986e265636069036e3047c1538e
Marius <mariusthiesset@gmail.com>
2024-11-12 15:32:54 +0100

    Fix build #2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  // Commenter ou enlever la propriété `content` pour désactiver la purge
+  // content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-  safelist: [
-    "bg-indigo-600",
-    "text-center",
-    "sm:text-4xl",
-    "animate-spin",
-    "hidden",
-  ],
+  // Vous pouvez également enlever ou commenter `safelist` car elle n'est plus nécessaire
+  // safelist: [
+  //   "bg-indigo-600",
+  //   "text-center",
+  //   "sm:text-4xl",
+  //   "animate-spin",
+  //   "hidden",
+  // ],
 };
