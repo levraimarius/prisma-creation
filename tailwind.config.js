0250324a75f7adc9af805531faf77d9a5499cbf2
Marius <mariusthiesset@gmail.com>
2024-11-12 15:36:47 +0100

    Fix tailwind class

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // Commenter ou enlever la propriété `content` pour désactiver la purge
-  // content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-  // Vous pouvez également enlever ou commenter `safelist` car elle n'est plus nécessaire
-  // safelist: [
-  //   "bg-indigo-600",
-  //   "text-center",
-  //   "sm:text-4xl",
-  //   "animate-spin",
-  //   "hidden",
-  // ],
 };
