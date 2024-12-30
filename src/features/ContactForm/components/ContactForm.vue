2594800a39a8568233eb8a4541af76379056dd8b
Marius <mariusthiesset@gmail.com>
2024-12-30 14:46:12 +0100

    Fix build

--- src/features/ContactForm/components/ContactForm.vue
+++ src/features/ContactForm/components/ContactForm.vue
@@ -53,7 +53,7 @@
       <div class="flex justify-end">
         <button
           type="submit"
-          :disabled="isLoading"
+          :disabled="props.isLoading"
           class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-white transition-all duration-200 bg-indigo-600 rounded-lg hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed"
         >
           <PaperAirplaneIcon
